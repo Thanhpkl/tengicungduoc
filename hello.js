ad0f2eb7a3becc3af4d68fff377dd739ca1560e0
Thanhpkl <dotienthanhthanh6@gmail.com>
2022-03-30 20:15:14 +0700

    Code để tỏ tình này minh :))

---
+++ hello.js
@@ -0,0 +1,10 @@
+let ngoc = document.getElementById("ngoc")
+
+function Continue(){
+    if(ngoc.textContent = " Bảo Ngọc" || ngoc.textContent == "Trần Bảo Ngọc"){
+        alert("You are ngọc")
+        location.replace("")
+    }else{
+        alert("You are not Ngọc")
+    }
+}
